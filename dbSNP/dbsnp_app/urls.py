d64168e383d1c1814961159944d22085b16a2b07
sophie22 <sophie22@users.noreply.github.com>
2022-03-03 20:58:38 +0000

    add 'results' to url and template

--- dbSNP/dbsnp_app/urls.py
+++ dbSNP/dbsnp_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index"),
+	path("", views.home, name="home"),
 	path("home/", views.home, name="home"),
+	path("results/", views.results, name="results")
 
 ]
 
