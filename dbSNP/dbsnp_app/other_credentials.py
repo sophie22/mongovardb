84ce5bbec6949a5bd9701aa2f48d806efc12b046
sophie22 <sophie22@users.noreply.github.com>
2022-03-04 10:34:16 +0000

    Delete other_credentials.py

--- dbSNP/dbsnp_app/other_credentials.py
+++
@@ -1,8 +0,0 @@
-# Credentials for MongoDB server
-mongodb_cred = {
-    'host': 'mango.btppp.mongodb.net',
-    'username': 'user_31',
-    'password': 'vd5iKoqTzg7iG6Sm',
-    'database': 'testDB',
-    'collection': 'inventory'
-}
