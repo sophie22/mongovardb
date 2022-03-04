933ec436bc0f4d698bc1452cfafaa5749221c6d3
sophie22 <sophie22@users.noreply.github.com>
2022-03-04 10:25:37 +0000

    fix typo

--- dbSNP/dbsnp_app/urls.py
+++ dbSNP/dbsnp_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 	path("", views.home, name="home"),
 	path("home/", views.home, name="home"),
-	path("results/", views.results, name="results")
+	path("results/", views.results, name="results"),
 
 ]
 
